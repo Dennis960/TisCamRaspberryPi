fba40c8ede5f3f9c1e11c84b0680256bf8e9b0ab
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-23 10:15:26 +0200

    update

--- main.py
+++ main.py
@@ -7,48 +7,41 @@ import cv2
 import ctypes
 import numpy as np
 
+def convert(buf):
+    if not buf:
+        return None
+    pixel_format = buf.get_image_pixel_format()
+    bits_per_pixel = pixel_format >> 16 & 0xff
+    if bits_per_pixel == 8:
+        INTP = ctypes.POINTER(ctypes.c_uint8)
+    else:
+        INTP = ctypes.POINTER(ctypes.c_uint16)
+    addr = buf.get_data()
+    ptr = ctypes.cast(addr, INTP)
+    im = np.ctypeslib.as_array(ptr, (buf.get_image_height(), buf.get_image_width()))
+    im = im.copy()
+    return im
+
 camera=Aravis.Camera()
 print("Camera found: ", camera.get_device_id())
 
 stream=camera.create_stream(None,None)
 stream.push_buffer(Aravis.Buffer.new_allocate(camera.get_payload()))
 camera.start_acquisition()
-buf=stream.pop_buffer()
-print(buf)
-
-def convert(buf):
-	if not buf:
-		return None
-	pixel_format = buf.get_image_pixel_format()
-	bits_per_pixel = pixel_format >> 16 & 0xff
-	if bits_per_pixel == 8:
-		INTP = ctypes.POINTER(ctypes.c_uint8)
-	else:
-		INTP = ctypes.POINTER(ctypes.c_uint16)
-	addr = buf.get_data()
-	ptr = ctypes.cast(addr, INTP)
-	im = np.ctypeslib.as_array(ptr, (buf.get_image_height(), buf.get_image_width()))
-	im = im.copy()
-	return im
-	
-print("Start acquisition")
-stream=camera.create_stream(None,None)
-stream.push_buffer(Aravis.Buffer.new_allocate(camera.get_payload()))
-camera.start_acquisition()
 
 while True:
-	buffer = stream.try_pop_buffer()
-	print(buffer) 
-	if buffer:
-		frame = convert(buffer)
-		stream.push_buffer(buffer) #push buffer back into stream
+    buf = stream.pop_buffer()
+    print(buf) 
+    if buf:
+        frame = convert(buf)
+        stream.push_buffer(buf) #push buffer back into stream
 
-		cv2.imshow("frame", frame)
-		ch = cv2.waitKey(1) & 0xFF
-		if ch == 27 or ch == ord('q'):
-			break
-		elif ch == ord('s'):
-			cv2.imwrite("imagename.png",frame)
+        cv2.imshow("frame", frame)
+        ch = cv2.waitKey(1) & 0xFF
+        if ch == 27 or ch == ord('q'):
+            break
+        elif ch == ord('s'):
+            cv2.imwrite("imagename.png",frame)
 
 
 camera.stop_acquisition()
