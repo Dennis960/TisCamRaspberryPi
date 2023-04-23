399c10bb459664294dc9859522979f267647c25e
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-23 10:07:35 +0200

    update

--- main.py
+++ main.py
@@ -1,56 +1,13 @@
-# Ararvis python videcapture
 import gi
-gi.require_version("Aravis", "0.8")
+gi.require_version('Aravis','0.8')
 from gi.repository import Aravis
 
-import cv2
-import ctypes
-import numpy as np
+cam=Aravis.Camera()
+dev=cam.get_device()
 
-Aravis.update_device_list()
-camera = Aravis.Camera.new(None)
-print("Found camera with name: " + camera.get_device_id())
-stream = camera.create_stream(None, None)
-
-payload = camera.get_payload()
-
-print("Payload: " + str(payload))
-print("Stream: " + str(stream))
-
-# for i in range(0,50):
-# 	stream.push_buffer(Aravis.Buffer.new_allocate(payload))
-
-# def convert(buf):
-# 	if not buf:
-# 		return None
-# 	pixel_format = buf.get_image_pixel_format()
-# 	bits_per_pixel = pixel_format >> 16 & 0xff
-# 	if bits_per_pixel == 8:
-# 		INTP = ctypes.POINTER(ctypes.c_uint8)
-# 	else:
-# 		INTP = ctypes.POINTER(ctypes.c_uint16)
-# 	addr = buf.get_data()
-# 	ptr = ctypes.cast(addr, INTP)
-# 	im = np.ctypeslib.as_array(ptr, (buf.get_image_height(), buf.get_image_width()))
-# 	im = im.copy()
-# 	return im
-	
-# print("Start acquisition")
-# camera.start_acquisition()
-
-# while True:
-# 	buffer = stream.try_pop_buffer()
-# 	print(buffer) 
-# 	if buffer:
-# 		frame = convert(buffer)
-# 		stream.push_buffer(buffer) #push buffer back into stream
-
-# 		cv2.imshow("frame", frame)
-# 		ch = cv2.waitKey(1) & 0xFF
-# 		if ch == 27 or ch == ord('q'):
-# 			break
-# 		elif ch == ord('s'):
-# 			cv2.imwrite("imagename.png",frame)
-
-
-# camera.stop_acquisition()
+if 1:
+    stream=cam.create_stream(None,None)
+    stream.push_buffer(Aravis.Buffer.new_allocate(cam.get_payload()))
+    cam.start_acquisition()
+    buf=stream.pop_buffer()
+    print(buf)
