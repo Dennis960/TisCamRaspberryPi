366cf0e6a2096edab99ded7e827b6a65cc644ff0
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-23 10:01:44 +0200

    update

--- main.py
+++ main.py
@@ -8,45 +8,48 @@ import ctypes
 import numpy as np
 
 Aravis.update_device_list()
-camera = Aravis.Camera.new(None)
-stream = camera.create_stream(None, None)
+Aravis.update_device_list()
+devices = Aravis.get_n_devices()
+print(devices)
+# camera = Aravis.Camera.new(None)
+# stream = camera.create_stream(None, None)
 
-payload = camera.get_payload()
+# payload = camera.get_payload()
 
-for i in range(0,50):
-	stream.push_buffer(Aravis.Buffer.new_allocate (payload))
+# for i in range(0,50):
+# 	stream.push_buffer(Aravis.Buffer.new_allocate(payload))
 
-def convert(buf):
-	if not buf:
-		return None
-	pixel_format = buf.get_image_pixel_format()
-	bits_per_pixel = pixel_format >> 16 & 0xff
-	if bits_per_pixel == 8:
-		INTP = ctypes.POINTER(ctypes.c_uint8)
-	else:
-		INTP = ctypes.POINTER(ctypes.c_uint16)
-	addr = buf.get_data()
-	ptr = ctypes.cast(addr, INTP)
-	im = np.ctypeslib.as_array(ptr, (buf.get_image_height(), buf.get_image_width()))
-	im = im.copy()
-	return im
+# def convert(buf):
+# 	if not buf:
+# 		return None
+# 	pixel_format = buf.get_image_pixel_format()
+# 	bits_per_pixel = pixel_format >> 16 & 0xff
+# 	if bits_per_pixel == 8:
+# 		INTP = ctypes.POINTER(ctypes.c_uint8)
+# 	else:
+# 		INTP = ctypes.POINTER(ctypes.c_uint16)
+# 	addr = buf.get_data()
+# 	ptr = ctypes.cast(addr, INTP)
+# 	im = np.ctypeslib.as_array(ptr, (buf.get_image_height(), buf.get_image_width()))
+# 	im = im.copy()
+# 	return im
 	
-print("Start acquisition")
-camera.start_acquisition()
+# print("Start acquisition")
+# camera.start_acquisition()
 
-while True:
-	buffer = stream.try_pop_buffer()
-	print(buffer) 
-	if buffer:
-		frame = convert(buffer)
-		stream.push_buffer(buffer) #push buffer back into stream
+# while True:
+# 	buffer = stream.try_pop_buffer()
+# 	print(buffer) 
+# 	if buffer:
+# 		frame = convert(buffer)
+# 		stream.push_buffer(buffer) #push buffer back into stream
 
-		cv2.imshow("frame", frame)
-		ch = cv2.waitKey(1) & 0xFF
-		if ch == 27 or ch == ord('q'):
-			break
-		elif ch == ord('s'):
-			cv2.imwrite("imagename.png",frame)
+# 		cv2.imshow("frame", frame)
+# 		ch = cv2.waitKey(1) & 0xFF
+# 		if ch == 27 or ch == ord('q'):
+# 			break
+# 		elif ch == ord('s'):
+# 			cv2.imwrite("imagename.png",frame)
 
 
-camera.stop_acquisition()
+# camera.stop_acquisition()
